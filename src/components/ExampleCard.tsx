5ee73457aafb94c7435d2dc920e07c7530204360
33357 <im33357dr@gmail.com>
2025-08-08 12:42:58 +0800

    修正

--- src/components/ExampleCard.tsx
+++ src/components/ExampleCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ColorfulAddress } from "./ColorfulAddress";
 import { Checkbox } from "./CheckBox";
 import { motion } from "framer-motion";
